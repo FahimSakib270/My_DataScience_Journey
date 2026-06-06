f8f2f19443fbfddefc13bc6a78892eab7849e63e
FahimX <sakibfahim270@gmail.com>
2026-06-06 23:30:38 +0800

    for loop

---
+++ Python/ControlFlowAndLoops/loops/for_loop/for_loop.py
@@ -0,0 +1,12 @@
+for i in range(1,6):
+    print(i)
+
+for i in range(2,7):
+    print("the number is ",i)
+
+for i in range(1,10,2):
+    print(i)
+
+for i in range(1,11):
+    print(f"5X{i} =",i*5)
+
