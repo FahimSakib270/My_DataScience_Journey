4ad40912d2c671523b04f90fd5c091f73320f3d0
FahimX <sakibfahim270@gmail.com>
2026-06-09 23:02:16 +0800

    sets and method

---
+++ Python/Data Structures in Python/Sets and Set Methods/Sets and Set Methods.py
@@ -0,0 +1,17 @@
+fruits = {"apple","orange","mango"}
+
+my_set = {1,2,3,4,5}
+my_set.add(7)
+print(my_set)
+my_set.remove(3)
+print(my_set)
+
+my_set.pop()
+print(my_set)
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
---
+++ Python/Data Structures in Python/Tuples and Operations on Tuples/Tuples and Operations on Tuples.py
@@ -0,0 +1,10 @@
+my_tuple = (1,2,3,4)
+single_tuple = (4,)
+
+print(my_tuple[2])
+
+a,b,c,d = my_tuple
+print(a,b)
+
+print(my_tuple.count(3))
+print(my_tuple.index(2))
