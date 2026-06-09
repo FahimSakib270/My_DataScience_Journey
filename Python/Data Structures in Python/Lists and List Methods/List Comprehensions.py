9fbbdba18e419187b9d873b5976384cef9acdafb
FahimX <sakibfahim270@gmail.com>
2026-06-09 22:44:22 +0800

    list comprehensions

---
+++ Python/Data Structures in Python/Lists and List Methods/List Comprehensions.py
@@ -0,0 +1,4 @@
+squared = [x**2 for x in range(5)]
+
+sqare = [x**3 for x in range(6)]
+print(sqare)
---
+++ Python/Data Structures in Python/Lists and List Methods/list&Method.py
@@ -0,0 +1,15 @@
+numbers = [1,2,3,4,5]
+mixed = [1,True,"Fahim",3.5]
+
+numbers.append(21)
+print(numbers)
+numbers.insert(1,56)
+print(numbers)
+numbers.remove(3)
+print(numbers)
+numbers.pop()
+print(numbers)
+mixed.reverse()
+print(mixed)
+numbers.sort()
+print(numbers)
